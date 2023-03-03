bc52a1af8cdc1689c2832281f4f25c2804c8c21b
Eline Suijten <elinesuijten@live.nl>
2023-03-03 13:19:18 +0100

    Finish RPS project

--- script.js
+++ script.js
@@ -13,12 +13,25 @@ rockButton.addEventListener("click", clickFunction);
 paperButton.addEventListener("click", clickFunction);
 scissorsButton.addEventListener("click", clickFunction);
 
+rockButton.addEventListener("click", clickFunction);
+paperButton.addEventListener("click", clickFunction);
+scissorsButton.addEventListener("click", clickFunction);
+
+
 function clickFunction(e) {
     const computerSelection = getComputerChoice(arrayRPS);
     const playerSelectionId = e.target.id;
     const playerSelection = playerSelectionId.slice(0, -6);
     document.getElementById('roundresult').innerHTML = playRound(playerSelection, computerSelection);
     document.getElementById('score').innerHTML = `Score: ${playerScore} - ${computerScore}`
+    if (computerSelection === 'rock') {
+        document.getElementById('compButton').style = "background-image: url('rock.png'); background-size: 80%;"
+    } else if (computerSelection === 'paper') {
+        document.getElementById('compButton').style = "background-image: url('paper.png'); background-size: 85%;"
+    } else {
+        document.getElementById('compButton').style = "background-image: url('scissors.png'); background-size: 90%;"
+    }
+
     if (playerScore === 5) {
         document.getElementById('endscore').innerHTML = `Game over: You won!!`
         disableButtons()
@@ -28,17 +41,21 @@ function clickFunction(e) {
     }
 }
 
+
 function disableButtons() {
     rockButton.disabled = true;
+    document.getElementById('rockButton').style = "pointer-events: none;"
     paperButton.disabled = true;
+    document.getElementById('paperButton').style = "pointer-events: none;"
     scissorsButton.disabled = true;
+    document.getElementById('scissorsButton').style = "pointer-events: none;"
 }
+
 
 function getComputerChoice() {
     const computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
     return computerChoice;
 }
-
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
